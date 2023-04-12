d4fdd83c43c79d539dc29ee6755ef04dab97b980
piupaulgit <contactpiu619@gmail.com>
2023-04-13 00:36:14 +0530

    side srwader icon

--- myShare/components/SideDrawer.tsx
+++ myShare/components/SideDrawer.tsx
@@ -1,13 +1,19 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerActions } from '@react-navigation/native';
+import { Avatar, Pressable, Text } from 'native-base';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
 
 const Drawer = createDrawerNavigator();
 
-function SideDrawer() {
+function SideDrawer(props:any) {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
+    <Drawer.Navigator screenOptions={{
+      drawerPosition: 'left',
+      headerLeft: () => <Pressable onPress={() => props.navigation.dispatch(DrawerActions.openDrawer())}><Avatar bg="gray.700">PP</Avatar></Pressable>,
+  }}>
+    <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Profile" component={Profile} />
     </Drawer.Navigator>
   );
 }
